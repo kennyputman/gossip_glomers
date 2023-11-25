7c04f26458b09f3255076ed47f9d1d7965a48965
Kenny Putman <kbputman@gmail.com>
2023-11-25 15:40:58 -0600

    finish echo

--- echo/EchoServer.cpp
+++ echo/EchoServer.cpp
@@ -7,24 +7,46 @@ void EchoServer::run() {
     while (std::getline(std::cin, line)) {
         json data = json::parse(line);
 
-        fmt::print(stderr, "Received {}\n", data.dump());
+        auto msg = data.template get<Message>();
 
-        if (data["body"]["type"] == "init") {
-            node_id = data["body"]["node_id"];
+        std::string type = msg.body["type"];
+        if (type == "init") {
+            node_id = msg.body["node_id"];
+            ++next_msg_id;
+
+            Message response;
+            response.src = node_id;
+            response.dest = msg.src;
+            response.body["msg_id"] = next_msg_id;
+            response.body["in_reply_to"] = msg.body["msg_id"];
+            response.body["type"] = "init_ok";
+
+            json j = response;
+            fmt::print(stdout, "{}\n", j.dump());
+
+        } else if (type == "echo") {
+            ++next_msg_id;
+
+            Message response;
+            response.src = node_id;
+            response.dest = msg.src;
+            response.body["type"] = "echo_ok";
+            response.body["msg_id"] = next_msg_id;
+            response.body["in_reply_to"] = msg.body["msg_id"];
+            response.body["echo"] = msg.body["echo"];
+
+            json j = response;
+            fmt::print(stdout, "{}\n", j.dump());
         }
-
-        fmt::print(stderr, "Initialized node {}", node_id);
     }
 }
 
-// {
-//     "body" : {
-//         "msg_id" : 1,
-//         "node_id" : "n1",
-//         "node_ids" : ["n1"],
-//         "type" : "init"
-//     },
-//              "dest" : "n1",
-//                       "id" : 0,
-//                       "src" : "c0"
-// }
+void to_json(json &j, const Message &m) {
+    j = json{{"src", m.src}, {"dest", m.dest}, {"body", m.body}};
+}
+
+void from_json(const json &j, Message &m) {
+    j.at("src").get_to(m.src);
+    j.at("dest").get_to(m.dest);
+    j.at("body").get_to(m.body);
+}
--- echo/include/EchoServer.hpp
+++ echo/include/EchoServer.hpp
@@ -5,9 +5,21 @@
 #include "fmt/core.h"
 #include "nlohmann/json.hpp"
 #include <iostream>
+#include <optional>
 #include <string>
 
 using json = nlohmann::json;
+
+struct Message {
+    std::string src;
+    std::string dest;
+    json body;
+
+    Message() = default;
+};
+
+void to_json(json &j, const Message &m);
+void from_json(const json &j, Message &m);
 
 class EchoServer {
   public:
@@ -18,4 +30,5 @@ class EchoServer {
 
   private:
     std::string node_id;
+    int next_msg_id = 0;
 };
