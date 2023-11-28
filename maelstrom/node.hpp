72a088fe2997f8bfc509d3d8502651d1e14bc078
Kenny Putman <kbputman@gmail.com>
2023-11-27 18:50:33 -0600

    add ability to register handlers on the node class

--- maelstrom/node.hpp
+++ maelstrom/node.hpp
@@ -36,17 +36,17 @@ class Node {
     void run() {
         std::string line;
         while (std::getline(std::cin, line)) {
+
             json data = json::parse(line);
-
             auto msg = data.template get<Message>();
-
             std::string type = msg.body["type"];
+
+            ++next_msg_id;
             if (type == "init") {
                 node_id = msg.body["node_id"];
-                ++next_msg_id;
 
                 Message response;
-                response.src = node_id;
+                response.src = msg.dest;
                 response.dest = msg.src;
                 response.body["msg_id"] = next_msg_id;
                 response.body["in_reply_to"] = msg.body["msg_id"];
@@ -54,27 +54,37 @@ class Node {
 
                 json j = response;
                 fmt::print(stdout, "{}\n", j.dump());
+            } else {
+                if (handlers.find(type) != handlers.end()) {
+                    handlers[type](msg);
+                } else {
+                    Message response;
+                    response.src = node_id;
+                    response.dest = msg.src;
+                    response.body["type"] = "error";
+                    response.body["msg_id"] = next_msg_id;
+                    response.body["in_reply_to"] = msg.body["msg_id"];
+                    response.body["code"] = 10;
+                    response.body["text"] =
+                        "unknown message type: [" + type + "]";
 
-            } else if (type == "echo") {
-                ++next_msg_id;
-
-                Message response;
-                response.src = node_id;
-                response.dest = msg.src;
-                response.body["type"] = "echo_ok";
-                response.body["msg_id"] = next_msg_id;
-                response.body["in_reply_to"] = msg.body["msg_id"];
-                response.body["echo"] = msg.body["echo"];
-
-                json j = response;
-                fmt::print(stdout, "{}\n", j.dump());
+                    json j = response;
+                    fmt::print(stdout, "{}\n", j.dump());
+                }
             }
         }
     }
 
+    void handle(std::string type, std::function<void(const Message)> handler) {
+        handlers[type] = handler;
+    }
+
+    int get_next_msg_id() { return next_msg_id; }
+
   private:
     std::string node_id;
     int next_msg_id = 0;
+    std::unordered_map<std::string, std::function<void(Message)>> handlers;
 };
 
 } // namespace maelstrom
--- src/echo/echo.cpp
+++ src/echo/echo.cpp
@@ -3,6 +3,20 @@
 
 int main(int argc, char *argv[]) {
     auto node = std::make_shared<maelstrom::Node>();
+
+    node->handle("echo", [&](maelstrom::Message msg) {
+        maelstrom::Message response;
+        response.src = msg.dest;
+        response.dest = msg.src;
+        response.body["type"] = "echo_ok";
+        response.body["msg_id"] = node->get_next_msg_id();
+        response.body["in_reply_to"] = msg.body["msg_id"];
+        response.body["echo"] = msg.body["echo"];
+
+        json j = response;
+        fmt::print(stdout, "{}\n", j.dump());
+    });
+
     node->run();
     return 0;
 }
