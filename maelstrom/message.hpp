4633b74ab3df19c750b1f8e71fc0bae9ba1c84e7
Kenny Putman <kbputman@gmail.com>
2023-11-27 18:52:47 -0600

    move message into its own file

---
+++ maelstrom/message.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "fmt/core.h"
+#include "nlohmann/json.hpp"
+#include <iostream>
+#include <optional>
+#include <string>
+
+using json = nlohmann::json;
+
+namespace maelstrom {
+
+struct Message {
+    std::string src;
+    std::string dest;
+    json body;
+
+    Message() = default;
+};
+
+void to_json(json &j, const Message &m) {
+    j = json{{"src", m.src}, {"dest", m.dest}, {"body", m.body}};
+}
+
+void from_json(const json &j, Message &m) {
+    j.at("src").get_to(m.src);
+    j.at("dest").get_to(m.dest);
+    j.at("body").get_to(m.body);
+}
+} // namespace maelstrom
