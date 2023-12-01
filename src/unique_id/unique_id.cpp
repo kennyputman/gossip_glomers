a33003af332dcd8d9495d9c4f881cfb0bb386908
Kenny Putman <kbputman@gmail.com>
2023-12-01 15:39:17 -0600

    finish unique id generation challenge

---
+++ src/unique_id/unique_id.cpp
@@ -0,0 +1,37 @@
+
+#include "maelstrom/message.hpp"
+#include "maelstrom/node.hpp"
+#include <fmt/core.h>
+#include <iostream>
+#include <uuid.h>
+
+int main(int argc, char const *argv[]) {
+    std::random_device rd;
+    auto seed_data = std::array<int, std::mt19937::state_size>{};
+    std::generate(std::begin(seed_data), std::end(seed_data), std::ref(rd));
+    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+    std::mt19937 generator(seq);
+
+    auto node = std::make_shared<maelstrom::Node>();
+
+    node->handle("generate", [&](maelstrom::Message msg) {
+        maelstrom::Message response;
+        response.src = msg.dest;
+        response.dest = msg.src;
+        response.body["type"] = "generate_ok";
+        response.body["msg_id"] = node->get_next_msg_id();
+        response.body["in_reply_to"] = msg.body["msg_id"];
+
+        uuids::uuid_random_generator gen{generator};
+        uuids::uuid const id = gen();
+        std::string uuid_str = uuids::to_string(id);
+
+        response.body["id"] = uuid_str;
+
+        json j = response;
+        fmt::print(stdout, "{}\n", j.dump());
+    });
+
+    node->run();
+    return 0;
+}
